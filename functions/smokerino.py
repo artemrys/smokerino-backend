02745c3364209b93f38a609e92bc8085610fb375
Artem Rys <rysartem@gmail.com>
2020-03-22 17:38:35 +0100

    Added check for blank user_id.

--- functions/smokerino.py
+++ functions/smokerino.py
@@ -5,12 +5,19 @@ import flask
 from google.cloud import firestore
 
 
+def build_firestore_client() -> firestore.Client:
+    return firestore.Client()
+
+
 def get_cigarette(request: flask.Request) -> flask.Response:
     if request.method != "GET":
         return flask.make_response(flask.make_response("Only GET requests"),
                                    405)
-    firestore_client = firestore.Client()
+    firestore_client = build_firestore_client()
     user_id = request.args.get("user_id")
+    if user_id is None:
+        return flask.make_response(flask.Response("User should not be blank"),
+                                   400)
     cigarette_ref = firestore_client.collection(f"cigarettes/{user_id}/smoked")
     smoked_cigarettes = len(list(cigarette_ref.get()))
     logging.info("User {user_id} asked about his progress")
@@ -21,8 +28,11 @@ def get_cigarette(request: flask.Request) -> flask.Response:
 def add_cigarette(request: flask.Request):
     if request.method != "POST":
         return flask.make_response(flask.Response("Only POST requests"), 405)
-    firestore_client = firestore.Client()
+    firestore_client = build_firestore_client()
     user_id = request.args.get("user_id")
+    if user_id is None:
+        return flask.make_response(flask.Response("User should not be blank"),
+                                   400)
     user_id_ref = firestore_client.collection("cigarettes").document(user_id)
     user_id_ref.set({
         "user_id": user_id,
--- main_test.py
+++ main_test.py
@@ -1,7 +1,8 @@
-from unittest.mock import Mock
+from unittest import mock
 
 import flask
 import pytest
+from google.cloud import firestore
 
 import main
 
@@ -12,17 +13,44 @@ def app():
     return flask.Flask(__name__)
 
 
+@pytest.fixture(scope="function")
+def firestore_client():
+    mock_firestore_client = mock.MagicMock(firestore.Client)
+    build_firestore_client_patcher = mock.patch(
+        "functions.smokerino.build_firestore_client",
+        return_value=mock_firestore_client)
+    build_firestore_client_patcher.start()
+    yield build_firestore_client_patcher
+    build_firestore_client_patcher.stop()
+
+
 @pytest.mark.parametrize("method", ["POST", "PUT", "DELETE"])
 def test_get_cigarette_returns_not_allowed_if_not_get_request(app, method):
-    request_mock = Mock(method=method)
+    request_mock = mock.Mock(method=method)
     with app.test_request_context():
         response = main.get_cigarette_request(request_mock)
     assert response.status_code == 405
 
 
+def test_get_cigarette_returns_bad_request_when_no_user_id(
+        app, firestore_client):
+    request_mock = mock.Mock(method="GET", args={})
+    with app.test_request_context():
+        response = main.get_cigarette_request(request_mock)
+    assert response.status_code == 400
+
+
 @pytest.mark.parametrize("method", ["GET", "PUT", "DELETE"])
 def test_add_cigarette_returns_not_allowed_if_not_post_request(app, method):
-    request_mock = Mock(method=method)
+    request_mock = mock.Mock(method=method)
     with app.test_request_context():
         response = main.add_cigarette_request(request_mock)
     assert response.status_code == 405
+
+
+def test_add_cigarette_returns_bad_request_when_no_user_id(
+        app, firestore_client):
+    request_mock = mock.Mock(method="POST", args={})
+    with app.test_request_context():
+        response = main.add_cigarette_request(request_mock)
+    assert response.status_code == 400
